80790edab647fafb3e78d2b96017a7fbd66de13f
URK21CS3013 <109952237+URK21CS3013@users.noreply.github.com>
2022-07-25 14:59:13 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,33 +1,49 @@
-import os
-class Stack:
-    def __init__(self, size):
-        self.items = []
-        self.size = size
-
-    def is_empty(self):
-        # Write code here
-
-    def is_full(self):
-        # Write code here
-
-    def push(self, data):
-        if not self.is_full():
-            # Write code here
-
-    def pop(self):
-        if not self.is_empty():
-            # Write code here
-
-    def status(self):
-        # Write code here
-
-# Do not change the following code
-size, queries = map(int, input().rstrip().split())
-stack = Stack(size)
-for line in range(queries):
-    values = list(map(int, input().rstrip().split()))
-    if values[0] == 1:
-        stack.push(values[1])
-    elif values[0] == 2:
-        stack.pop()
-stack.status()
+class stack2:
+    def __init__(s,n):
+        s.n=n
+        s.st=[None]*n
+        s.top=-1
+        
+    def isfull(s):
+        return s.top==s.n-1
+        
+    def push(s):
+        if s.isfull():
+            print("STACK IS FULL")
+        else:
+            s.top+=1
+            x=int(input("Enter data : "))
+            s.st[s.top]=x
+    def display(s):
+        for i in range (s.top+1):
+            print(s.st[i])
+    def pop(s):
+        if s.top==-1:
+            print("Stack is empty")
+        else:
+            print("The deleted element is : ",s.st[s.top])
+            s.st[s.top]=None
+            s.top-=1
+    def exit(s):
+        print("See u soon")
+        
+       
+        
+size=int(input("Enter the size of the stack : "))
+s1=stack2(size)
+while (1):
+    print('''1-PUSH
+    2-POP
+    3-DISPLAY
+    4-EXIT''')
+    ch=int(input("Enter your choice : "))
+    if ch==1:
+        s1.push()
+    elif ch==2:
+        s1.pop()
+    elif ch==3:
+        s1.display()
+    elif ch==4:
+        break
+    else:
+        print("INVALID CHOICE:")
